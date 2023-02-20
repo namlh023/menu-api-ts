13151ba7ddf1ada30fea7e412158a6b2fd373dd4
namlh <namlh@rikkeisoft.com>
2023-02-20 11:14:26 +0700

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,15 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
 
 dotenv.config();
 
 if (!process.env.PORT) {
-    process.exit(1);
+  process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10)
+const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
@@ -21,6 +22,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/menu/items", itemsRouter);
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+  console.log(`Listening on port ${PORT}`);
+});
